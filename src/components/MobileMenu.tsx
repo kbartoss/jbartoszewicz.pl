4c56cedcabe713897b4efeda56934ab25c7883b2
Jakub Bartoszewicz <jakub.bartoszewicz@codeagency.pl>
2025-09-15 13:18:29 +0200

    adjust mobile menu width

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -44,7 +44,7 @@ const MobileMenu: React.FC<MobileMenuProps> = ({ menuItems, currentPath, classNa
       </button>
 
       <div
-        className={`fixed top-0 right-0 z-50 h-full w-80 transform bg-white shadow-xl transition-transform duration-300 ease-in-out ${
+        className={`fixed top-0 right-0 z-50 h-full w-[70%] transform bg-white shadow-xl transition-transform duration-300 ease-in-out ${
           isOpen ? 'translate-x-0' : 'translate-x-full'
         }`}
       >
